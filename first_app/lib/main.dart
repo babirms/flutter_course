dc653faf44531d2102373c750dd2d2c2a5c76354
babirms <bramos@id.uff.br>
2020-01-21 16:26:03 -0300

    refactoring homepage - white background

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,100 +1,14 @@
-import 'dart:math';
-
+// os códigos do app devem ser executados dentro da função 'main'
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-  
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: MyHomePage(title: 'Frutas do Dia'),
-    );
-  }
-}
+  // comando que executa o app; utiliza uma instância de classe como argumento
+  runApp(MaterialApp(
+    title: 'Frutas do Dia',
+    home: Container(
+      color: Colors.white,
+    ),
+  ));
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _numeroAleatorio= 0;
-  List _frutas = [
-    'Morango  🍓',
-    'Banana 🍌',
-    'Melancia 🍉',
-    'Limão 🍋'
-  ];
-
-  void _incrementCounter() {
-    setState(() {
-  
-    // gera um número aleatório e atribui a variável _numeroAleatorio
-      _numeroAleatorio = new Random().nextInt(4);
-      
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pressione o botão para selecionar uma fruta:' ,
-            ),
-            Text(
-              _frutas[_numeroAleatorio],
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
